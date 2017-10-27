8f3f43c92bf97c0fadc3a1e68347d3c9d1c64cfc
Dan Guest <dguest@cern.ch>
2017-10-27 17:08:58 +0200

    Add more meghan kernel

--- plot-meghan.py
+++ plot-meghan.py
@@ -24,7 +24,7 @@ def run():
     with File(args.input_file,'r') as h5file:
         x, y = get_xy_pts(h5file['Nominal']['mjj_Data_2015_3p57fb'])
 
-    valid_x = (x > 1200) & (x < 7000)
+    valid_x = (x > 1200) & (x < 5000)
     valid_y = y > 0
     valid = valid_x & valid_y
     x, y = x[valid], y[valid]
@@ -34,8 +34,10 @@ def run():
     lnProb = logLike_minuit(x, y, xerr)
     min_likelihood, best_fit = fit_gp_minuit(1, lnProb)
 
-    kernel_new = get_kernel((best_fit['Amp'], best_fit['lengthscale']))
-    p0, p1, p2 = [best_fit[x] for x in ['p0','p1','p2']]
+    fit_pars = ['p0','p1','p2']
+    kargs = {x:y for x, y in best_fit.items() if x not in fit_pars}
+    kernel_new = get_kernel(**kargs)
+    p0, p1, p2 = [best_fit[x] for x in fit_pars]
     gp_new = george.GP(kernel_new, mean=Mean((p0,p1,p2)), fit_mean = True)
 
     gp_new.compute(x, yerr)
@@ -60,13 +62,13 @@ class logLike_minuit:
         self.x = x
         self.y = y
         self.xerr = xerr
-    def __call__(self, Amp, lengthscale, p0, p1, p2):
-        kernel = get_kernel((Amp, lengthscale))
+    def __call__(self, Amp, decay, length, power, sub, p0, p1, p2):
+        kernel = get_kernel(Amp, decay, length, power, sub)
         mean = Mean((p0,p1,p2))
         gp = george.GP(kernel, mean=mean, fit_mean = True)
         try:
             gp.compute(self.x, np.sqrt(self.y))
-            return -gp.lnlikelihood(self.y)
+            return -gp.lnlikelihood(self.y, self.xerr)
         except:
             return 0
 
@@ -77,20 +79,37 @@ def fit_gp_minuit(num, lnprob):
     best_fit_params = (0, 0, 0, 0, 0)
     for i in range(num):
         iamp = np.random.random() * 1e10
-        ilengthscale = np.random.random() * 5e5
-        ip0 = np.random.random() * 1.
-        ip1 = np.random.random() * 1.
-        ip2 = np.random.random() * 1.
-        m = Minuit(lnprob, throw_nan = False, pedantic = False,
+        idecay = np.random.random() * 0.64
+        ilength = np.random.random() * 5e5
+        ipower = np.random.random() * 1.0
+        isub = np.random.random() * 1.0
+        ip0 = np.random.random() * 1.0
+        ip1 = np.random.random() * 1.0
+        ip2 = np.random.random() * 1.0
+        m = Minuit(lnprob, throw_nan = True, pedantic = False,
                    print_level = 0, errordef = 0.5,
-                   Amp = iamp, lengthscale = ilengthscale,
+                   Amp = iamp,
+                   decay = idecay,
+                   length = ilength,
+                   power = ipower,
+                   sub = isub,
                    p0 = ip0, p1 = ip1, p2 = ip2,
-                   error_Amp = 1e1, error_lengthscale = 1e1,
+                   error_Amp = 1e1,
+                   error_decay = 1e1,
+                   error_length = 1e1,
+                   error_power = 1e1,
+                   error_sub = 1e1,
                    error_p0 = 1e-2, error_p1 = 1e-2, error_p2 = 1e-2,
-                   limit_Amp = (0.01, 1e15), limit_lengthscale = (100, 1e8),
+                   limit_Amp = (0.01, 1e15),
+                   limit_decay = (0.01, 1000),
+                   limit_length = (100, 1e8),
+                   limit_power = (0.01, 1000),
+                   limit_sub = (0.01, 1e6),
                    limit_p0 = (0,10),
-                   limit_p1 = (-2e2, 2e2), limit_p2 = (-2e2,2e2))
+                   limit_p1 = (-2e2, 2e2),
+                   limit_p2 = (-2e2,2e2))
         m.migrad()
+        print(m.fval)
         if m.fval < min_likelihood:
             min_likelihood = m.fval
             best_fit_params = m.values
@@ -108,9 +127,8 @@ class Mean():
         p0, p1, p2 = self.p0, self.p1, self.p2
         return (p0 * (1.-t/sqrts)**p1 * (t/sqrts)**(p2))*np.append(np.diff(t), np.diff(t)[-1])
 
-def get_kernel(hyperparams):
-    from george.kernels import ExpSquaredKernel
-    return hyperparams[0]*ExpSquaredKernel(hyperparams[1])
+def get_kernel(Amp, decay, length, power, sub):
+    return Amp * MyDijetKernelSimp(a = decay, b = length, c = power, d = sub)
 
 def get_xy_pts(group):
     assert 'hist_type' in group.attrs
